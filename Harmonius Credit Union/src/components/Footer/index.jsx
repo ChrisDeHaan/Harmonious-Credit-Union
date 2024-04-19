1b322d8a90dc0a3493bb5540839b8613c3e4177a
Chris DeHaan <christopherwdehaan@gmail.com>
2024-04-19 13:38:08 -0400

    started building homepage and created a header/footer component

---
+++ Harmonius Credit Union/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+export default function Footer () {
+    return (
+        <>
+            <footer className="bg-gradient-to-br from-violet-600 to-cyan-950 h-14">
+                <div className="text-center text-white">
+                    This is a footer.
+                </div>
+            </footer>
+        </>
+    )
+}
